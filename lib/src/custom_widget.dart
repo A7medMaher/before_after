8bfe774dbb66b8fc0b6ff356944656a52cfc19f9
Ahmed Maher Mostfa <35171375+A7medMaher@users.noreply.github.com>
2020-10-29 14:37:29 +0300

    corneres adjustments

--- lib/src/custom_widget.dart
+++ lib/src/custom_widget.dart
@@ -41,8 +41,8 @@ class _BeforeAfterState extends State<BeforeAfter> {
       children: <Widget>[
         Padding(
           padding: widget.isVertical
-              ? const EdgeInsets.symmetric(vertical: 24.0)
-              : const EdgeInsets.symmetric(horizontal: 24.0),
+              ? const EdgeInsets.symmetric(vertical: 1.0)
+              : const EdgeInsets.symmetric(horizontal: 1.0),
           child: SizedImage(
             widget.afterImage,
             widget.imageHeight,
@@ -52,8 +52,8 @@ class _BeforeAfterState extends State<BeforeAfter> {
         ),
         Padding(
           padding: widget.isVertical
-              ? const EdgeInsets.symmetric(vertical: 24.0)
-              : const EdgeInsets.symmetric(horizontal: 24.0),
+              ? const EdgeInsets.symmetric(vertical: 1.0)
+              : const EdgeInsets.symmetric(horizontal: 1.0),
           child: ClipPath(
             clipper: widget.isVertical
                 ? RectClipperVertical(_clipFactor)
@@ -137,7 +137,9 @@ class CustomThumbShape extends SliderComponentShape {
       RenderBox parentBox,
       SliderThemeData sliderTheme,
       TextDirection textDirection,
-      double value}) {
+      double value,
+      double textScaleFactor,
+      Size sizeWithOverflow}) {
     final Canvas canvas = context.canvas;
 
     final Paint paint = Paint()
